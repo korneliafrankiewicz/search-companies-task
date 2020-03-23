639a767b132ae642d2a3669db5e100e3f804ee28
korneliafrankiewicz <nelcia6991@gmail.com>
2020-03-23 11:32:36 +0100

    first commit - adding files

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./App.css";
+import FetchCompanies from "./FetchCompanies";
+import { Icon } from "semantic-ui-react";
+
+class App extends React.Component {
+  state = {
+    searchValue: " "
+  };
+
+  filterData(value) {
+    this.setState({ searchValue: value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <input
+              className="search"
+              placeholder="Search..."
+              type="text"
+              onChange={event => this.filterData(event.target.value)}
+            />
+          </div>
+        </header>
+        <body>
+          <FetchCompanies searchValue={this.state.searchValue} />
+        </body>
+        <footer className="App-footer">
+          <>
+            <p>
+              <Icon className="mail-icon" name="mail outline" />
+              If you have any questions, please feel free to contact us at
+              professional-company@gmail.com
+            </p>
+          </>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/FetchCompanies.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Image } from "semantic-ui-react";
+
+export default class FetchComanies extends React.Component {
+  props = {
+    searchValue: ""
+  };
+
+  state = {
+    data: []
+  };
+
+  async componentDidMount() {
+    const url = "https://recruitment.hal.skygate.io/companies";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ data: data });
+  }
+
+  render() {
+    return (
+      <div className="companies">
+        {this.state.data
+          .filter(item => item.name.includes(this.props.searchValue))
+          .map((item, index) => (
+            <>
+              <div className="company">
+                <Image
+                  src="https://d3dbtvmfwwhlf2.cloudfront.net/pub/Marketing/WEBSITE+images/Hero+pictures/kemppi-company-facts-hero.jpg?c=hero_jpg"
+                  size="large"
+                />
+                <p>
+                  <b>Name: </b>
+                  {item.name}{" "}
+                </p>
+                <p>
+                  <b>City: </b>
+                  {item.city}
+                </p>
+                <p>
+                  <b>Identity number: </b>
+                  {item.id}{" "}
+                </p>
+              </div>
+            </>
+          ))}
+      </div>
+    );
+  }
+}
